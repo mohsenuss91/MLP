e67a98a87ab69ba225887839c48e0bc1d94f4cf9
eebeer <kimduckhwan@gatech.edu>
2014-11-04 00:35:06 -0500

    comment

--- BP.m
+++ BP.m
@@ -2,7 +2,7 @@ function [W,B,Err] = BP(train_output,Act,W,B,num_layer,lrn_rate,Err)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
-
+%Err=local gradient
 prev_err = (train_output - Act{end});
 for k=num_layer-1:-1:1
     Err{k} = Act{k+1}.*(1-Act{k+1}).*prev_err;
